e90f52e5b017f66c480094a735b12032af75051c
AdubiEmmanuel <emmanueladubi24@gmail.com>
2023-05-23 11:55:39 +0100

    stuff

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function Hero() {
     return (
       <div>
-        <h3>Hero section</h3>
+        <main className="h-auto w-100 p-20 bg-slate-200  mt-0">
+          <h3 className="divide-slate-700 bg-clip-text text-transparent bg-gradient-to-r from-pink-950 to-violet-800  text-3xl justify-center text-center">
+            Mc Timmy Variety Foods Sells The best Nigerian Food
+          </h3>
+        </main>
       </div>
     );
 }
--- src/pages/Utilities/Navbar.jsx
+++ src/pages/Utilities/Navbar.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
-      <h3>Navbar</h3>
+      <nav className="bg-slate-900 flex flex-row justify-center text-white h-20 p-6 gap-8 w-100">
+        <ul className="flex gap-10 text-white">
+          <li className="relative right-60 text-3xl font-bold">
+            McTimmy Variety Foods
+          </li>
+          <li>
+            <Link to="/about" className="text-white decoration- hover:text-grey-500">About us</Link>
+          </li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </div>
   );
 }

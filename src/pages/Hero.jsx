10e5d7586b7fcceb47ab45e75ab09f2f97e8f5e1
AdubiEmmanuel <emmanueladubi24@gmail.com>
2023-05-24 12:14:23 +0100

    video

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -5,29 +5,42 @@ import MainContent from './MainContent';
 export default function Hero() {
     return (
       <div>
-         <main 
-              data-controller='className-toggler' 
-              data-className-toggler-active-toggler-classNamees-value='["bg-white", "border-gray-200", "shadow-sm"]'
-              data-className-toggler-inactive-toggler-classNamees-value='["border-transparent", "hover:text-gray-700"]'>
-              <section className="px-4 py-8 leading-6 text-gray-700 sm:px-6 lg:px-8">
-                <div className="w-full mx-auto duration-500 ease-in-out transform max-w-screen-3xl" data-className-toggler-target="toggleable">
-                  <div className="relative">
-                    <div className="overflow-hidden shadow-xl rounded-xl " style={{transformOrigin:"right center"}}>
-                      <div className='flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-xl h-7'>
-                        <span className='w-2 h-2 bg-white rounded-full'></span>
-                        <span className='w-2 h-2 bg-white rounded-full'></span>
-                        <span className='w-2 h-2 bg-white rounded-full'></span>
-                      </div>
-                      <div
-                        className="rounded-b-xl w-full align-middle h-[calc(100vh_-_10.75rem)] p-5" 
-                      >
-                        <h3>Hero section</h3>
-                      </div>
-                    </div>
+        <main
+          data-controller="className-toggler"
+          data-className-toggler-active-toggler-classNamees-value='["bg-white", "border-gray-200", "shadow-sm"]'
+          data-className-toggler-inactive-toggler-classNamees-value='["border-transparent", "hover:text-gray-700"]'
+        >
+          <section className="px-4 py-8 leading-6 text-gray-700 sm:px-6 lg:px-8">
+            <div
+              className="w-full mx-auto duration-500 ease-in-out transform max-w-screen-3xl"
+              data-className-toggler-target="toggleable"
+            >
+              <div className="relative">
+                <div
+                  className="overflow-hidden shadow-xl rounded-xl "
+                  style={{ transformOrigin: "right center" }}
+                >
+                  <div className="flex items-center pl-3 space-x-1 bg-gray-200 rounded-t-xl h-7">
+                    <span className="w-2 h-2 bg-white rounded-full"></span>
+                    <span className="w-2 h-2 bg-white rounded-full"></span>
+                    <span className="w-2 h-2 bg-white rounded-full"></span>
+                  </div>
+                  <div className="rounded-b-xl w-full align-middle h-[calc(100vh_-_10.75rem)] p-5">
+                    <iframe
+                      width="560"
+                      height="315"
+                      src="https://www.youtube.com/embed/4brRKwiW3Ns"
+                      title="YouTube video player"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowfullscreen
+                    ></iframe>
                   </div>
                 </div>
-              </section>
-            </main>
+              </div>
+            </div>
+          </section>
+        </main>
       </div>
     );
 }
